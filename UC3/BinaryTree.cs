e0e02885a816d87d06f3d021f7c78d69c01cfd08
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:29:20 +0530

    remove 63 in binary search tree

---
+++ UC3/BinaryTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3
+{
+    class BinaryTree
+    {
+        public Node root = null;
+
+        public void Add(int i)
+        {
+            Node node = new Node();
+            node.data = i;
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            parent.LeftNode = node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = node;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(node.data + " : Data is added");
+        }
+
+        public void Traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Traverse(root.LeftNode);
+            Console.Write(root.data + " ");
+            Traverse(root.RightNode);
+
+        }
+        public bool Search(Node root, int value)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            if (root.data == value)
+            {
+                return true;
+            }
+            else if (root.data > value)
+            {
+                return Search(root.LeftNode, value);
+            }
+            else
+            {
+                return Search(root.RightNode, value);
+            }
+        }
+    }
+}
---
+++ UC3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+            BinaryTree binarytree = new BinaryTree();
+
+            binarytree.Add(56);
+            binarytree.Add(30);
+            binarytree.Add(70);
+            binarytree.Add(22);
+            binarytree.Add(40);
+            binarytree.Add(60);
+            binarytree.Add(95);
+            binarytree.Add(11);
+            binarytree.Add(65);
+            binarytree.Add(3);
+            binarytree.Add(16);
+            binarytree.Add(63);
+            binarytree.Add(67);
+
+            
+            binarytree.Traverse(binarytree.root);
+            Console.WriteLine();
+
+            bool isFound = binarytree.Search(binarytree.root, 63);
+            Console.WriteLine("63 is found " + isFound);
+        }
+    }
+}
