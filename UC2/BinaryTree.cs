6055e4850e4637511e30bae2e3ae0097d381e45e
nikhita <nikhitajamadar1998@gmail.com>
2022-02-27 15:27:53 +0530

    Adding elements in binary search tree

---
+++ UC2/BinaryTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    class BinaryTree
+    {
+        public Node root = null;
+
+        public void Add(int i)
+        {
+            Node node = new Node();
+            node.data = i;
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.data)
+                    {
+                        current = current.LeftNode;
+                        if (current == null)
+                        {
+                            parent.LeftNode = node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        if (current == null)
+                        {
+                            parent.RightNode = node;
+                            break;
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine(node.data + " : Data is added");
+        }
+
+        public void Traverse(Node root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Traverse(root.LeftNode);
+            Console.Write(root.data + " ");
+            Traverse(root.RightNode);
+
+        }
+    }
+}
